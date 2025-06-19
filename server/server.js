edb4d7894c6469c5af6c313dc8e7532dccc65768
lordb0n <156414661+lordb0n@users.noreply.github.com>
2025-06-20 02:11:38 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ app.use('/api/chats', chatRoutes);
  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
 //});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
