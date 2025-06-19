cf3c919a8a967500dd474707e0d9824f6128f46e
lordb0n <156414661+lordb0n@users.noreply.github.com>
2025-06-20 02:32:48 +0300

    Update1 server.js

--- server/server.js
+++ server/server.js
@@ -66,10 +66,10 @@ app.use('/api/chats', chatRoutes);
 
 
 // Статичні файли фронтенду (якщо є)
-//app.use(express.static(path.join(__dirname, '../client/build')));
-//app.get('*', (req, res) => {
- // res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//});
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
